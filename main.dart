0c480955386135df13f1fc45bc29b4cf0d8f692d
Fadi Bdiwi <fadibdiwi98@yahoo.de>
2024-09-06 17:18:33 +0200

    add main file

---
+++ main.dart
@@ -0,0 +1,29 @@
+import "dart:io";
+
+import "aufgabe_1.dart";
+import "aufgabe_2.dart";
+import "aufgabe_3.dart";
+import "aufgabe_4.dart";
+
+void main() {
+  print("Bitte wähle eine dieser OPtionen aus...");
+  print("1. Erste-Registration");
+  print("2. Anmelden");
+  print("3. Produkte scannen");
+  print("4. Liste mit Personen erstellen");
+
+  String selectOption = stdin.readLineSync()!;
+  switch (selectOption) {
+    case "1" || "Erste-Registration":
+      firstRegistration();
+    case "2" || "Anmelden":
+      loginUser();
+    case "3" || "Produkte scannen":
+      scanProduct();
+    case "4" ||
+          "Liste mit Personen erstellen" ||
+          "Liste erstellen" ||
+          "Personen hinzufügen":
+      listWithPeople();
+  }
+}
